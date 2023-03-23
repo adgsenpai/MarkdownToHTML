c2f3640047a49edcc00bb5c1f6a974c5866ca013
adgsenpai <adg@adgstudios.co.za>
2023-03-23 12:31:35 +0200

    Completed Parser

--- MarkdownToHTML.py
+++ MarkdownToHTML.py
@@ -1,8 +1,10 @@
 import markdown
+import pdfkit 
 
 def markdown_to_html(markdown_text):
     return markdown.markdown(markdown_text, extensions=['markdown.extensions.fenced_code', 'markdown.extensions.codehilite', 'markdown.extensions.tables'])
 
+# This is the template for the html file change the template.html to your liking
 def CompileMarkdownToHTML(markdown_text,filename):
     html = markdown_to_html(markdown_text)
     with open("template.html", "r") as f:
@@ -13,7 +15,20 @@ def CompileMarkdownToHTML(markdown_text,filename):
     with open(filename, "w") as f:
         f.write(template)
 
+def HTMLTOPDF(filename):     
+    # some weird retarded error pdfkit is giving but it works? thats the purpose of the try and catch 
+    try:
+        pdfkit.from_file(filename, filename.split(".")[0]+".pdf")
+    except:
+        pass
+    
+
 if __name__ == "__main__":
+    # You may run the hello world example here?
     with open("helloworld.md", "r") as f:
         markdown_text = f.read()
-    CompileMarkdownToHTML(markdown_text,"helloworld.html")    
+    CompileMarkdownToHTML(markdown_text,"helloworld.html")       
+    HTMLTOPDF("helloworld.html") 
+
+    
+ 
---
+++ README.md
@@ -0,0 +1,13 @@
+# ADGSTUDIOS Markdown to HTML Converter - HTML to PDF Converter
+
+I had problems using 8 converters to convert my markdown files to HTML and PDF. So I decided to make my own. This is a simple converter that converts markdown files to HTML and PDF. It is written in Python. Motivation for my implementation was the extension MarkdownToPDF for Visual Studio Code i researched everything about it and reversed engineered how they did it. Quite some cool stuff in there. I used the standard markdown library. To convert the markdown into html with some extensions. 
+
+## Installation
+    This software is cross-platform should work on Windows, Linux and Mac but make sure you set up the libraries properly
+
+   - You should install https://wkhtmltopdf.org/ and add it to your path.
+   - You should install Python 3.7 or higher
+    - You should install the requirements.txt file with pip install -r requirements.txt
+
+## Usage
+    python3 MarkdownToHTML.py    
